f78b53af9f9bce71f3d1f2e18bce0524b74d4a42
ndegeulaya <ndegeulayabaton@gmail.com>
2025-06-15 02:22:54 +0300

    view result noe

--- src/js/views/resultView.js
+++ src/js/views/resultView.js
@@ -1,8 +1,44 @@
-class resultView{
-#result;
+import view from "./view.js";
+import icons from 'url:../../img/icons.svg';
+
+class resultView extends view{
+_parentElement = document.querySelector('.results');
+
+_errorMessage='no recipe found try another one!!!!';
 
 
-    renderResult(){
 
-    }
+
+_returnHtml(){
+
+    console.log(this._data);
+
+    return this._data.map(this._returnHtmlPreview).join('');
+
+   
 }
+ _returnHtmlPreview(result){
+ return`
+     <li class="preview">
+            <a class="preview__link" href="#${result.id}">
+              <figure class="preview__fig">
+                <img src="${result.image}" alt="Test" />
+              </figure>
+              <div class="preview__data">
+                <h4 class="preview__title">${result.title}</h4>
+                <p class="preview__publisher">${result.publisher}</p>
+                <div class="preview__user-generated">
+                  <svg>
+                    <use href="${icons}#icon-user"></use>
+                  </svg>
+                </div>
+              </div>
+            </a>
+          </li>
+    `
+ }
+
+   
+}
+
+export default new resultView();
