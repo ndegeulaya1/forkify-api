f395028a9bcd7fdf6df2b9af197794b46bd79a9d
ndegeulaya <ndegeulayabaton@gmail.com>
2025-06-22 20:38:29 +0300

    icons

--- dist/js-final.4a59a05f.js
+++ dist/js-final.4a59a05f.js
@@ -1634,14 +1634,16 @@ class recipeView extends (0, _viewJsDefault.default) {
 
            <div class="recipe__info-buttons">
             <button class="btn--tiny btn--update-servings" data-update-to="${this._data.serving - 1}">
-              <svg>
-                <use href="${0, _iconsSvgDefault.default}#icon-minus-circle"></use>
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+
             </button>
             <button class="btn--tiny btn--update-servings" data-update-to="${this._data.serving + 1}">
-              <svg>
-                <use href="${0, _iconsSvgDefault.default}#icon-plus-circle"></use>
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+
             </button>
           </div>
         </div>
